38874d77219821afd2d711932ba7cb039acd173a
ldhernandezmx <119636476+ldhernandezmx@users.noreply.github.com>
2023-01-24 09:45:07 -0600

    💡 add comments in PositionServices

--- src/main/java/com/sa/erp/services/PositionService.java
+++ src/main/java/com/sa/erp/services/PositionService.java
@@ -15,20 +15,40 @@ public class PositionService {
     @Autowired
     private PositionRepository positionRepo;
 
+    /**
+     *
+     * @return a list of position
+     */
     public List<Position> getAllPositions(){
         return positionRepo.findAll();
     }
 
+    /**
+     *
+     * @param id
+     * @return Optional of position
+     */
     public Optional<Position> getPositionById(String id){
         return positionRepo.findById(id);
     }
 
+    /**
+     *
+     * @param position to save
+     * @return position saved
+     */
     public Position savePosition(Position position){
         position.setCreateDate(LocalDate.now());
         position.setUpdateDate(LocalDate.now());
         return positionRepo.save(position);
     }
 
+    /**
+     *
+     * @param id of position to update
+     * @param position body with the datas to update
+     * @return position updated
+     */
     public Position updatePosition(String id, Position position){
         Optional<Position> aux = this.getPositionById(id);
         if(aux.isPresent()){
@@ -40,6 +60,11 @@ public class PositionService {
         return null;
     }
 
+    /**
+     *
+     * @param id of the position to delete
+     * @return the position deleted
+     */
     public Position deletePosition(String id){
         Optional<Position> aux = this.getPositionById(id);
         if(aux.isPresent()){
